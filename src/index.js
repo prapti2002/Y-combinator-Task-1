4cc689cd9a243850df900cd6485de6f78bfe063c
prapti2002 <prapti.dt@gmail.com>
2023-06-15 16:17:21 +0530

    adding latest changes

--- src/index.js
+++ src/index.js
@@ -4,3 +4,32 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Function to handle drag start event
+function handleDragStart(event) {
+    event.target.classList.add('dragging');
+  }
+  
+  // Function to handle drag over event
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
+  
+  // Function to handle drop event
+  function handleDrop(event) {
+    event.preventDefault();
+    const card = document.querySelector('.dragging');
+    const targetSwimlane = event.target.closest('.swimlane');
+    targetSwimlane.appendChild(card);
+    card.classList.remove('dragging');
+    card.classList.add('changed');
+  }
+  
+  // Add event listeners to cards
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+    card.addEventListener('dragstart', handleDragStart);
+    card.addEventListener('dragover', handleDragOver);
+    card.addEventListener('drop', handleDrop);
+  });
+  
